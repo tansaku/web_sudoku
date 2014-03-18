418074b9d249562e5fc336ff279b471dd253ea17
Mario Gintili <mariojgintili@gmail.com>
2014-03-18 14:06:52 +0000

    implemented crazy method by evgeny

--- sudoku.rb
+++ sudoku.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require_relative './lib/sudoku'
 require_relative './lib/cell'
 
+	enable :sessions
+
 	def random_sudoku
 		seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
 		sudoku = Sudoku.new(seed.join)
@@ -9,7 +11,39 @@ require_relative './lib/cell'
 		sudoku.to_s.chars
 	end
 
+	def puzzle(sudoku)
+		# this is what I'm supposed to implement
+		sudoku
+	end
+
+	def box_order_to_row_order(cells)
+		boxes = cells.each_slice(9).to_a
+		(0..8).to_a.inject([]) { |memo, i|
+			first_box_index = i / 3 * 3
+			three_boxes = boxes[first_box_index, 3]
+			three_rows_of_three = three_boxes.map do |box|
+				row_number_in_a_box = i % 3
+				first_cell_in_the_row_index = row_number_in_a_box * 3
+				box[first_cell_in_the_row_index, 3]
+			end
+			memo += three_rows_of_three.flatten }
+	end
+
 	get '/' do
+		sudoku = random_sudoku
+		session[:solution] = sudoku
 		@current_solution = random_sudoku
 		erb :index
+	end
+
+	get '/solution' do
+	  @current_solution = session[:solution]
+	  erb :index
+	end
+
+	post '/' do
+		cells = params["cell"]
+		session[:current_solution] = cells.map {|value| value.to_i }.join
+		session[:check_solution] = true
+		redirect to("/")
 	end
